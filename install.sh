35abe988d80f40f82ee9ffbf2e46a9693b6bb634
Benoît Grélard <benoit.grelard@gmail.com>
2019-06-05 13:58:24 -0400

    Add tower2

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ brew cask install iterm2
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
+brew cask install tower2
 brew cask install visual-studio-code
 brew cask install vlc
 
